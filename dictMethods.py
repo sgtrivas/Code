79a346b20601cdb27b1ba2f75976c9b282fbc48c
sgtrivas <ssgn.rivas@gmail.com>
2023-09-07 15:23:53 -0400

    CBTC Python Course

---
+++ dictMethods.py
@@ -0,0 +1,24 @@
+dict1 = {  "xxlx":"xx!xx",
+    "xfx":"ExLx",
+    "0":'xSx',
+     "www.":"SxQxUxIxRxRx",
+     "python.org":'xRxExT',
+     "pickle rick":'ExC' }
+
+#theKey = dict1.values()
+
+theKeyS = sorted(dict1.values(), reverse=True)
+#ans = theKeyS[5].replace('x','')
+#print(ans)
+#ans2 = theKeyS[0].replace('x','')
+#ans3 = theKeyS[1].replace('x','')
+#print(ans3)
+
+
+#ans = theKeyS[1][1],theKeyS[5][0],theKeyS[5][2],theKeyS[2][1],theKeyS[2][3],theKeyS[2][5],\
+#   theKeyS[3][0],theKeyS[3][2],theKeyS[3][4],theKeyS[3][6],theKeyS[3][10],theKeyS[3][10],\
+#   theKeyS[4][0],theKeyS[4][2],theKeyS[0][2]
+#theKeyS1 = theKeyS.values() 
+for i in theKeyS:
+    print(i.replace('x',''), end='' + "\n")
+#print(''.join(ans).replace('x',''))
