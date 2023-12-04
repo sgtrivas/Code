9a8afa0516e480a4e6a0d007da98af0880d02511
sgtrivas <ssgn.rivas@gmail.com>
2023-12-04 15:40:12 -0500

    Update dictMethods.py

--- dictMethods.py
+++ dictMethods.py
@@ -1,13 +1,29 @@
+
+### VERSION 2 - PFC Gonzalez - 23-018
 dict1 = {  "xxlx":"xx!xx",
     "xfx":"ExLx",
     "0":'xSx',
      "www.":"SxQxUxIxRxRx",
      "python.org":'xRxExT',
      "pickle rick":'ExC' }
+val1 = sorted(dict1)
+for i in val1:
+    print(dict1[i].replace('x',''), end="")
+
+
+
+
+
+#dict1 = {  "xxlx":"xx!xx",
+ #   "xfx":"ExLx",
+  #  "0":'xSx',
+   #  "www.":"SxQxUxIxRxRx",
+    # "python.org":'xRxExT',
+     #"pickle rick":'ExC' }
 
 #theKey = dict1.values()
 
-theKeyS = sorted(dict1.values(), reverse=True)
+#theKeyS = sorted(dict1.values(), reverse=True)
 #ans = theKeyS[5].replace('x','')
 #print(ans)
 #ans2 = theKeyS[0].replace('x','')
@@ -19,6 +35,6 @@ theKeyS = sorted(dict1.values(), reverse=True)
 #   theKeyS[3][0],theKeyS[3][2],theKeyS[3][4],theKeyS[3][6],theKeyS[3][10],theKeyS[3][10],\
 #   theKeyS[4][0],theKeyS[4][2],theKeyS[0][2]
 #theKeyS1 = theKeyS.values() 
-for i in theKeyS:
-    print(i.replace('x',''), end='' + "\n")
+#for i in theKeyS:
+  #  print(i.replace('x',''), end='' + "\n")
 #print(''.join(ans).replace('x',''))
