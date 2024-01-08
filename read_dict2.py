5949c5b31e52b1b4f372b4640e61aaed84e8dce9
sgtrivas <ssgn.rivas@gmail.com>
2024-01-08 15:14:50 -0500

    Update read_dict2.py

--- read_dict2.py
+++ read_dict2.py
@@ -1,19 +1,23 @@
+import pyinputplus as pyip
 dictionary = {}
 
 with open('Oxford_dictionary.txt', 'r') as f:
     for line in f:
         w = f.readline()
         x = line.strip().split('(')
-        d1 = {x[0].rstrip():x[1:]}
+        d1 = {x[0].rstrip():line}
         d2 = dictionary.update(d1)
+word = ''
 
-#print(len(dictionary))
-        
-#word = input("enter word to get the definition: ").title()
+while word != 'Q':
+    word = pyip.inputStr("Enter word to get the definition (enter q to stop): ").title()
+    if word in dictionary and word != 'Q':
+        for z in dictionary[word]:
+            definition = dictionary[z]
+            print(f'\nThe definition for the word {word} is \"{definition.rstrip()}\"')
+    elif word not in dictionary and word != 'Q':
+        print(f'\nThis {word} is not recognized in the Oxford Dictionary!')
+    if word == 'Q':
+        print(f'{word} Thank you, Goodbye')
+        break   
 
-#print(dictionary[word])
-
-a = dictionary.values()
-for i in a:
-    print(i)
-  
