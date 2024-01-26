2ff1a01e4fe419d81cfc05a59d71cd482a887708
sgtrivas <ssgn.rivas@gmail.com>
2024-01-26 14:53:59 -0500

    PCEP notes
    .py file with pcep notes

---
+++ PCEP.py
@@ -0,0 +1,27 @@
+# empty_tuple = ()
+# #one_element_tuple = tuple(1)
+# one_element_tuple = 1,
+# two_element_tuple = (1,2.5)
+# two_element_tuple = 1,2.5
+
+# print(two_element_tuple[0]*2)
+
+
+# answers = (True,False,True)
+# selection = answers[2:]
+# points = 0
+# for answer in selection[-1:]:
+#     print(selection)
+#     if answer:
+#         print(answer)
+#         points += 1
+# print(points)
+
+# train_speed = {"FlyingScotsman":201, "TGV":320, "Shinkansen":320}
+
+# for train in train_speed.values():
+#     print(str(train)[0],end=''
+
+
+list_one = [1,2]
+print(list_one[:][0])
