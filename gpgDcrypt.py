08ff1beef64970b7e17d848887443d985e5146ee
sgtrivas <ssgn.rivas@gmail.com>
2024-01-26 14:42:14 -0500

    gpgDcrypt.py

--- gpgDcrypt.py
+++ gpgDcrypt.py
@@ -4,14 +4,23 @@ import subprocess as run
 def import_sig(loc):
     global command 
     command = 'gpg'
-    return run.run([command, '--import '+loc])
+    return run.run(command+' --import '+loc, shell=True)
 
 def gpg_decode():
-    file_name = input("Enter the file you want to decode: ")
-    return run.run([command, '-d '+file_name])
+    
+    try:
+        file_name = input("Enter the file you want to decode: ")
+        print()
+        return run.run(command+' -d '+file_name, shell=True)
+            
+    except (OSError, SystemError, ValueError,NameError): 
+        raise
+        
 
+pkey_loc = input("Enter file path to public key: ")
 
-pkey_loc = r"/home/nestor.a.rivas78/Documents/CND practical/Messages_(2)/Messages/Butters.asc"
 import_sig(pkey_loc)
+print()
 print(gpg_decode())
-#pkey_loc = input("Enter file path to public key: ")
+print()
+
